3ff75971b8dc28e5953f07d21ebdb2f95366e6d8
289249342 <289249342@qq.com>
2019-04-26 19:05:01 +0800

    day1

--- webapp/My_orm.py
+++ webapp/My_orm.py
@@ -14,12 +14,12 @@ async def create_pool(loop, **kw):
     logging.info('create database connection  pool')
     global __pool
     __pool = await aiomysql.create_pool(
-        host=kw.get('host', ''),
+        host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
         db=kw['db'],
-        charset=kw.get('charset', 'utf-8'),
+        charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit', True),
         maxsize=kw.get('maxsize', 10),
         minsize=kw.get('minsize', 1),
@@ -126,7 +126,7 @@ class ModelMetaclass(type):
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mapping__'] = mappings
+        attrs['__mappings__'] = mappings
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaryKey
         attrs['__fields__'] = fields
@@ -165,75 +165,69 @@ class Model(dict, metaclass=ModelMetaclass):
                 setattr(self, key, value)
             return value
 
+    @classmethod
+    async def findAll(cls, where=None, args=None, **kw):
+        'find object by where clause'
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?,?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value:%s' % str(limit))
+        rs = await select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
 
-@classmethod
-async def findAll(cls, where=None, args=None, **kw):
-    'find object by where clause'
-    sql = [cls.__select__]
-    if where:
-        sql.append('where')
-        sql.append(where)
-    if args is None:
-        args = []
-    orderBy = kw.get('orderBy', None)
-    if orderBy:
-        sql.append('order by')
-        sql.append(orderBy)
-    limit = kw.get('limit', None)
-    if limit is not None:
-        sql.append('limit')
-        if isinstance(limit, int):
-            sql.append('?')
-            args.append(limit)
-        elif isinstance(limit, tuple) and len(limit) == 2:
-            sql.append('?,?')
-            args.extend(limit)
-        else:
-            raise ValueError('Invalid limit value:%s' % str(limit))
-    rs = await select(' '.join(sql), args)
-    return [cls(**r) for r in rs]
+    @classmethod
+    async def findNumber(cls, selectField, where=None, args=None):
+        'find number by select and where'
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = await select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['_num_']
 
+    @classmethod
+    async def find(cls, pk):
+        'find object by primary key'
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
 
-@classmethod
-async def findNumber(cls, selectField, where=None, args=None):
-    'find number by select and where'
-    sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
-    if where:
-        sql.append('where')
-        sql.append(where)
-    rs = await select(' '.join(sql), args, 1)
-    if len(rs) == 0:
-        return None
-    return rs[0]['_num_']
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warning('failed to insert record:affected rows:%s' % rows)
 
+    async def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warning('failed to update by primary key:affected rows:%s' % rows)
 
-@classmethod
-async def find(cls, pk):
-    'find object by primary key'
-    rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
-    if len(rs) == 0:
-        return None
-    return cls(**rs[0])
-
-
-async def save(self):
-    args = list(map(self.getValueOrDefault, self.__fields__))
-    args.append(self.getValueOrDefault(self.__primary_key__))
-    rows = await execute(self.__insert__, args)
-    if rows != 1:
-        logging.warning('failed to insert record:affected rows:%s' % rows)
-
-
-async def update(self):
-    args = list(map(self.getValue, self.__fields__))
-    args.append(self.getValue(self.__primary_key__))
-    rows = await execute(self.__update__, args)
-    if rows != 1:
-        logging.warning('failed to update by primary key:affected rows:%s' % rows)
-
-
-async def remove(self):
-    args = [self.getValue(self.__primary_key__)]
-    rows = await execute(self.__delete__, args)
-    if rows != 1:
-        logging.warning('failed to remove by primary key:affected rows:%s' % rows)
+    async def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warning('failed to remove by primary key:affected rows:%s' % rows)
---
+++ webapp/Test.py
@@ -0,0 +1,14 @@
+import My_orm
+from My_model import User, Blog, Comment
+import asyncio
+
+
+async def test(loop):
+    await My_orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
+    u = User(name='Test', email='test@example.com', passwd='884443', image='about:blank')
+    await u.save()
+
+if __name__=='__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
+    loop.close()
