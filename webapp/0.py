3047516303c278805d7a1f1ab253179a4c667d42
289249342 <289249342@qq.com>
2021-11-17 19:48:43 +0800

    2021-11-15

--- webapp/0.py
+++ webapp/0.py
@@ -6,4 +6,99 @@ import pandas as pd
 import os
 import datetime
 import calendar
+import requests
+import json
 
+cal_year = 1
+update = 0
+in_path = r'E:/BC/data/SBsb/'
+# SYB.index = pd.DatetimeIndex(SYB.index)
+today = datetime.date.today()
+sd = str(today.replace(year=today.year - cal_year))
+ed = str(today)
+url1 = "https://open.lixinger.com/api/a/index/fundamental"
+url2 = "https://open.lixinger.com/api/macro/national-debt"
+data1 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "startDate": sd,
+    "endDate": ed,
+    "stockCodes": [
+        "000016"
+    ],
+    "metricsList": [
+        "pe_ttm.y10.mcw"
+    ]
+}
+data2 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "startDate": sd,
+    "endDate": ed,
+    "stockCodes": [
+        "000300"
+    ],
+    "metricsList": [
+        "pe_ttm.y10.mcw"
+    ]
+}
+data3 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "startDate": sd,
+    "endDate": ed,
+    "stockCodes": [
+        "000905"
+    ],
+    "metricsList": [
+        "pb.y10.mcw"
+    ]
+}
+data4 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "areaCode": "cn",
+    "startDate": sd,
+    "endDate": ed,
+    "metricsList": [
+        "mir_y10"
+    ]
+}
+headers = {
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip,deflate, br, *"
+}
+
+s50 = json.loads(requests.post(url=url1, data=json.dumps(data1), headers=headers).text)
+sy = json.loads(requests.post(url=url2, data=json.dumps(data4), headers=headers).text)
+print(s50)
+
+def add_one(x):
+    return datetime.datetime.strptime(x, '%Y-%m-%d').date() + datetime.timedelta(days=1)
+
+
+def pick_data(r50, ry):
+    if 1:
+        p1, p2, p3, p4, p5, p6 = [], [], [], [], [], []
+        for i in range(len(r50['data'])):
+            p1.append(r50['data'][i]['date'].split('T')[0])
+            p3.append(r50['data'][i]['pe_ttm']['y10']['mcw']['cv'])
+
+        for iy in range(len(ry['data'])):
+            p4.append(ry['data'][iy]['date'].split('T')[0])
+            p5.append(ry['data'][iy]['mir_y10'])
+            p6 = map(add_one, p4)
+        print(p5)
+        print(p6)
+        d1 = pd.DataFrame(data={'pe_50': p3}, index=p1)
+        d2 = pd.DataFrame(data={'y_10': p5}, index=p6)
+        print(d1)
+        print(d2)
+        d1.index=pd.DatetimeIndex(d1.index)
+        d1.index.name='time'
+        d2.index=pd.DatetimeIndex(d2.index)
+        d2.index.name = 'time'
+        print(d1.index)
+        print(d2.index)
+        d3 = pd.merge(d1,d2,on='time',how='inner')
+        print(d3)
+        return d3
+
+
+pick_data(s50, sy)
--- webapp/Stock_Bond_SYB.py
+++ webapp/Stock_Bond_SYB.py
@@ -4,185 +4,104 @@ __author__ = 'ARASHI'
 
 import pandas as pd
 import datetime
-import re
+import requests
+import json
 
 cal_year = 10
+update = 1
 in_path = r'E:/BC/data/SBsb/'
-SYB = pd.read_csv(in_path + 'SYB.csv', index_col=0)
 # SYB.index = pd.DatetimeIndex(SYB.index)
 today = datetime.date.today()
-SYB = SYB.loc[:str(today.replace(year=today.year - cal_year)), :]
-
-s = {
-    "code": 1,
-    "message": "success",
-    "data": [
-        {
-            "date": "2020-04-01T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.562802873848659
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-31T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.579863421617274
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-30T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.660476219465249
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-27T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.747137090562909
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-26T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.731245018454842
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-25T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.808484186310405
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-24T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.644815998954972
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-23T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.399040575794418
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-20T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.632676083610539
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-19T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.465137450271566
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-18T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.65082766931738
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-17T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.818375004439051
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-16T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.850836527313922
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-13T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.100211159217988
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-12T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.231228179400881
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-11T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.344932578606512
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-10T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.42828974391038
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-09T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.287756792232322
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-06T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.54247092585958
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-05T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.69261155312067
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-04T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.487072306857208
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-03T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.429466167729066
-            },
-            "stockCode": "000016"
-        },
-        {
-            "date": "2020-03-02T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 9.393348716478224
-            },
-            "stockCode": "000016"
-        }
+sd = str(today.replace(year=today.year - cal_year))
+ed = str(today)
+url1 = "https://open.lixinger.com/api/a/index/fundamental"
+url2 = "https://open.lixinger.com/api/macro/national-debt"
+data1 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "startDate": sd,
+    "endDate": ed,
+    "stockCodes": [
+        "000016"
+    ],
+    "metricsList": [
+        "pe_ttm.y10.mcw"
     ]
 }
-SYB = SYB.dropna(axis=0, how='any')
-if s['code']==1:
-    print(s['message'])
+data2 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "startDate": sd,
+    "endDate": ed,
+    "stockCodes": [
+        "000300"
+    ],
+    "metricsList": [
+        "pe_ttm.y10.mcw"
+    ]
+}
+data3 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "startDate": sd,
+    "endDate": ed,
+    "stockCodes": [
+        "000905"
+    ],
+    "metricsList": [
+        "pb.y10.mcw"
+    ]
+}
+data4 = {
+    "token": "17871030-d55c-4562-b166-2ac0b5682a0f",
+    "areaCode": "cn",
+    "startDate": sd,
+    "endDate": ed,
+    "metricsList": [
+        "mir_y10"
+    ]
+}
+headers = {
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip,deflate, br, *"
+}
+if update == 1:
+    s50 = json.loads(requests.post(url=url1, data=json.dumps(data1), headers=headers).text)
+    s300 = json.loads(requests.post(url=url1, data=json.dumps(data2), headers=headers).text)
+    s500 = json.loads(requests.post(url=url1, data=json.dumps(data3), headers=headers).text)
+    sy = json.loads(requests.post(url=url2, data=json.dumps(data4), headers=headers).text)
+
+# res = requests.post(url=url, data=json.dumps(data), headers=headers)
+# print(res.text)
+
+def add_one(x):
+    return datetime.datetime.strptime(x, '%Y-%m-%d').date() + datetime.timedelta(days=1)
+
+
+def pick_data(r50, r300, r500, ry):
+    if r50['code'] == 1:
+        p1, p2, p3, p4, p5, p6, p7 = [], [], [], [], [], [], []
+        for i in range(len(r50['data'])):
+            p1.append(r50['data'][i]['date'].split('T')[0])
+            p3.append(r50['data'][i]['pe_ttm']['y10']['mcw']['cv'])
+            p4.append(r300['data'][i]['pe_ttm']['y10']['mcw']['cv'])
+            p5.append(r500['data'][i]['pb']['y10']['mcw']['cv'])
+            p6.append(r500['data'][i]['pb']['y10']['mcw']['cvpos'])
+        for iy in range(len(ry['data'])):
+            p7.append(ry['data'][iy]['date'].split('T')[0])
+            p2.append(ry['data'][iy]['mir_y10'])
+        print(add_one(ry['data'][0]['date'].split('T')[0]))  # X****************************************************
+        print(len(p1), len(p2), len(p3), len(p4), len(p5), len(p6))
+        d1 = pd.DataFrame(data={'pe_50': p3, 'pe_300': p4, 'pb_500': p5, 'pre_500': p6}, index=p1)
+        d2 = pd.DataFrame(data={'y_10': p2}, index=map(add_one, p7))
+        d1.index = pd.DatetimeIndex(d1.index)
+        d1.index.name = 'time'
+        d2.index = pd.DatetimeIndex(d2.index)
+        d2.index.name = 'time'
+        d3 = pd.merge(d2, d1, on='time', how='inner')
+        return d3
+    else:
+        print(r50, r300, r500, sy)
+
+
+SYB = pick_data(s50, s300, s500, sy)
 SYB['syb_50'] = 1 / SYB['pe_50'] / SYB['y_10']
 SYB['syb_300'] = 1 / SYB['pe_300'] / SYB['y_10']
 print(SYB)
@@ -210,19 +129,6 @@ def cal_temp(da, axr=0):  # 将估值数据百分位化
 temp_50 = 100 - (cal_temp(list(SYB['syb_50'])[::-1]))  # 50股债收益比百分位
 temp_300 = 100 - (cal_temp(list(SYB['syb_300'])[::-1]))  # 300股债收益比百分位
 temp_500 = (cal_temp(list(SYB['pb_500'])[::-1]))  # 500PB百分位
-print('十年国债收益率  50股债收益比  50百分位   300股债收益比  300百分位   500PB    500百分位')
-print('%.3f          %.3f        %.2f      %.3f        %.2f      %.2f     %.2f' % (
+print('十年国债收益率  50股债收益比  50百分位   300股债收益比  300百分位   500PB    500百分位   '+str(today))
+print('%.3f          %.3f        %.2f     %.3f         %.2f      %.3f     %.2f' % (
     100 * SYB.iloc[0, 0], SYB.iloc[0, 5], temp_50, SYB.iloc[0, 6], temp_300, SYB.iloc[0, 3], temp_500))
-st = s['data'][0]['date']
-print(st.split('T')[0])
-'''s = {
-    "code": 1,
-    "message": "success",
-    "data": [
-        {
-            "date": "2020-04-01T00:00:00+08:00",
-            "pe_ttm": {
-                "mcw": 8.562802873848659
-            },
-            "stockCode": "000016"
-        },'''
